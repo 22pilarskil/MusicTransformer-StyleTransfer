a9445604e1952b02218f9694455918addca80b10
lmm <lpilarski3@gatech.edu>
2023-12-22 09:07:11 -0500

    Local changes

--- train.sh
+++ train.sh
@@ -1,12 +1,12 @@
 
 #TRAIN SCRIPT
 python3 train.py \
--continue_epoch 1 \
--continue_weights ../batching/weights/epoch_0021.pickle \
+-continue_epoch 0 \
+-continue_weights ../batching_new/weights/epoch_0004.pickle \
 -lr 0.0001 \
 -feature_size 128 \
 -input_dir ../dataset \
--output_dir ../batching_new \
+-output_dir ../batching_2.0_margin \
 -batch_size 5 \
 -n_workers 1 \
 -max_sequence 1000 \
